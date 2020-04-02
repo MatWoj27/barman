b684ba2f17a9c7299d54d2d3d8be35099f1bd374
Mateusz <226069@student.pwr.edu.pl>
2020-04-02 23:53:22 +0200

    Application repository with CRUD functionalities for recipes implemented

---
+++ app/src/main/java/com/mattech/barman/AppRepository.kt
@@ -0,0 +1,46 @@
+package com.mattech.barman
+
+import android.app.Application
+import com.mattech.barman.daos.RecipeDAO
+import com.mattech.barman.models.Recipe
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+class AppRepository : CoroutineScope {
+    private var recipeDAO: RecipeDAO
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    constructor(application: Application) {
+        recipeDAO = AppDatabase.getInstance(application)!!.getRecipeDAO()
+    }
+
+    fun getRecipes(category: String) = recipeDAO.getRecipes(category)
+
+    fun insertRecipe(recipe: Recipe) = launch { insertRecipeBG(recipe) }
+
+    private suspend fun insertRecipeBG(recipe: Recipe) {
+        withContext(Dispatchers.IO) {
+            recipeDAO.insertRecipe(recipe)
+        }
+    }
+
+    fun updateRecipe(recipe: Recipe) = launch { updateRecipeBG(recipe) }
+
+    private suspend fun updateRecipeBG(recipe: Recipe) {
+        withContext(Dispatchers.IO) {
+            recipeDAO.updateRecipe(recipe)
+        }
+    }
+
+    fun deleteRecipe(recipe: Recipe) = launch { deleteRecipeBG(recipe) }
+
+    private suspend fun deleteRecipeBG(recipe: Recipe) {
+        withContext(Dispatchers.IO) {
+            recipeDAO.deleteRecipe(recipe)
+        }
+    }
+}
