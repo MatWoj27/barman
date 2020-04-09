68a6436496dc419aa6f6d2f78e980aa1d62c5263
Mateusz <226069@student.pwr.edu.pl>
2020-04-09 23:49:32 +0200

    All functionalities of recipe view model implemented

--- app/src/main/java/com/mattech/barman/view_models/RecipeViewModel.kt
+++ app/src/main/java/com/mattech/barman/view_models/RecipeViewModel.kt
@@ -3,9 +3,16 @@ package com.mattech.barman.view_models
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.mattech.barman.AppRepository
+import com.mattech.barman.models.Recipe
 
 class RecipeViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = AppRepository(application)
 
     fun getRecipes(category: String) = repository.getRecipes(category)
+
+    fun addRecipe(recipe: Recipe) = repository.insertRecipe(recipe)
+
+    fun updateRecipe(recipe: Recipe) = repository.updateRecipe(recipe)
+
+    fun deleteRecipe(recipe: Recipe) = repository.deleteRecipe(recipe)
 }
