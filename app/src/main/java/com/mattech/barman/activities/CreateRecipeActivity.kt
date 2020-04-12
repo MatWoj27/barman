242aa8d2b91d627d4659dcbcab6b04c85be5d05f
Mateusz <226069@student.pwr.edu.pl>
2020-04-12 23:12:19 +0200

    Taking recipe photos implemented

--- app/src/main/java/com/mattech/barman/activities/CreateRecipeActivity.kt
+++ app/src/main/java/com/mattech/barman/activities/CreateRecipeActivity.kt
@@ -1,23 +1,38 @@
 package com.mattech.barman.activities
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mattech.barman.R
 import com.mattech.barman.adapters.IngredientAdapter
 import com.mattech.barman.adapters.IngredientListListener
+import com.mattech.barman.utils.CircleTransformation
 import kotlinx.android.synthetic.main.activity_create_recipe.*
 import kotlinx.android.synthetic.main.ingredients_edit_layout.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val IS_EDIT_TAG = "isEdit"
 
 class CreateRecipeActivity : AppCompatActivity(), IngredientListListener {
     private val DISPLAY_INGREDIENT_LIST_KEY = "displayIngredientList"
+    private val PHOTO_PATH_KEY = "photoPath"
+    private val REQUEST_TAKE_PHOTO = 1
     private var displayIngredientList = false
     private var isEdit: Boolean = true
     private var recipeId: Int = -1
+    private var photoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +45,29 @@ class CreateRecipeActivity : AppCompatActivity(), IngredientListListener {
         if (savedInstanceState != null && savedInstanceState.getBoolean(DISPLAY_INGREDIENT_LIST_KEY)) {
             showIngredientList()
         }
+        photoPath = savedInstanceState?.getString(PHOTO_PATH_KEY)
         cancel_btn.setOnClickListener { onCancelClick() }
+        add_photo.setOnClickListener { takePhoto() }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putBoolean(DISPLAY_INGREDIENT_LIST_KEY, displayIngredientList)
+        outState.putString(PHOTO_PATH_KEY, photoPath)
     }
 
     override fun onBackPressed() {
         onCancelClick()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            val bitmap = data?.extras?.get("data") as? Bitmap
+            if (bitmap != null) {
+                add_photo.setImageBitmap(CircleTransformation().transform(bitmap))
+            }
+        }
     }
 
     override fun lastItemRemoved() {
@@ -84,5 +112,30 @@ class CreateRecipeActivity : AppCompatActivity(), IngredientListListener {
                         .show()
             }
         }
+    }
+
+    private fun takePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            try {
+                val photo = createImageFile()
+                photoPath = photo.absolutePath
+                val photoUri = FileProvider.getUriForFile(this, "com.mattech.barman.fileprovider", photo)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+            } catch (ex: IOException) {
+                Log.e(javaClass.simpleName, "Failed to create a file for a photo: $ex")
+                Toast.makeText(this, getString(R.string.camera_error_toast), Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.no_camera_app_toast), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
     }
 }
