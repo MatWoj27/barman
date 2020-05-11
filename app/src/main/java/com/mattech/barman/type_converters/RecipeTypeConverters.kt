d2a01a0678ce3432c9bb06fcc80ee4f29456809d
Mateusz <226069@student.pwr.edu.pl>
2020-05-11 23:58:34 +0200

    Type converters return non-null values

--- app/src/main/java/com/mattech/barman/type_converters/RecipeTypeConverters.kt
+++ app/src/main/java/com/mattech/barman/type_converters/RecipeTypeConverters.kt
@@ -7,15 +7,12 @@ import com.google.gson.reflect.TypeToken
 class RecipeTypeConverters {
 
     @TypeConverter
-    fun arrayListToString(list: ArrayList<String>): String? {
+    fun arrayListToString(list: ArrayList<String>): String {
         return Gson().toJson(list)
     }
 
     @TypeConverter
-    fun stringToArrayList(data: String): ArrayList<String>? {
-        if (data == null) {
-            return null
-        }
+    fun stringToArrayList(data: String): ArrayList<String> {
         val type = object : TypeToken<ArrayList<String>>() {}.type
         return Gson().fromJson(data, type)
     }
