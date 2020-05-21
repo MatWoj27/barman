0990771ef99f6d3d2af2250cf3bc550008707b04
Mateusz <226069@student.pwr.edu.pl>
2020-05-21 19:17:26 +0200

    Quick refactoring

--- app/src/main/java/com/mattech/barman/utils/ImageUtil.kt
+++ app/src/main/java/com/mattech/barman/utils/ImageUtil.kt
@@ -16,10 +16,10 @@ fun Drawable.toBitmap(): Bitmap {
         return this.bitmap
     }
 
-    val bitmap = Bitmap.createBitmap(this.intrinsicWidth, this.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(bitmap)
-    this.setBounds(0, 0, canvas.width, canvas.height)
-    this.draw(canvas)
+    setBounds(0, 0, canvas.width, canvas.height)
+    draw(canvas)
 
     return bitmap
 }
